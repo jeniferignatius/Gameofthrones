de3d746db187b1c49c8cdff5d1f61780294c7cdd
Jeniferignatius <jeni.igni@gmail.com>
2021-07-16 14:01:49 +0300

    birthday images added

--- src/components/birthdays.jsx
+++ src/components/birthdays.jsx
@@ -7,18 +7,137 @@ export const Birthdays = (props) => {
             <h2>Birthdays</h2>
           </div>
           <div className='row'>
-            {props.data
-              ? props.data.map((d, i) => (
-                  <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+               
+               <div className='col-xs-6 col-md-3'> 
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
                     {' '}
-                    <a href ='allbirthdays.jsx'><i className={d.icon}></i></a>
-                    <h3>{d.title}</h3>
-                    <p>{d.text}</p>
+                    <a
+                      href='img/birthdays/birthday1.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='drawing2'
+                    >
+                      <span>
+                      <i className='fa fa-child'></i>
+                     </span>{' '}
+                     <div className='hover-text1'>
+                        <h4>First Birthday</h4>
+                        <p>We celebrated in finland 2016. We went to beach and the dress sent by Raja maama and dheepa athai.Cake bought from shop</p>
+                      </div>
+                    </a>{' '}
                   </div>
-                ))
-              : 'Loading...'}
+                </div>
+              </div>
+
+              <div className='col-xs-6 col-md-3'> 
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
+                    {' '}
+                    <a
+                      href='img/birthdays/birthday2.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='drawing2'
+                    >
+                      <span>
+                      <i className='fa fa-child'></i>
+                     </span>{' '}
+                     <div className='hover-text1'>
+                        <h4>Seocond Birthday</h4>
+                        <p>We celebrated in finland 2017 with her grandparents and the dress sent by her godparents .Cake made from home by her grandma and one more cake from Vidhya</p>
+                      </div>
+                    </a>{' '}
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-xs-6 col-md-3'> 
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
+                    {' '}
+                    <a
+                      href='img/birthdays/birthday3.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='drawing2'
+                    >
+                      <span>
+                      <i className='fa fa-child'></i>
+                     </span>{' '}
+                     <div className='hover-text1'>
+                        <h4>Third Birthday</h4>
+                        <p>We celebrated in finland 2018 with her grandparents. Went to ship and the dress bought by her grandparents.Cake made by her mary paatti </p>
+                      </div>
+                    </a>{' '}
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-xs-6 col-md-3'> 
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
+                    {' '}
+                    <a
+                      href='img/birthdays/birthday4.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='drawing2'
+                    >
+                      <span>
+                      <i className='fa fa-child'></i>
+                     </span>{' '}
+                     <div className='hover-text1'>
+                        <h4>Fourth Birthday</h4>
+                        <p>We celebrated in India 2019 with all our relatives and the dress from raja mama and her god parents.Peppa pig Cake bought from shop</p>
+                      </div>
+                    </a>{' '}
+                  </div>
+                </div>
+              </div>
+
+              <div className='col-xs-6 col-md-3'> 
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
+                    {' '}
+                    <a
+                      href='img/birthdays/birthday5.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='drawing2'
+                    >
+                      <span>
+                      <i className='fa fa-child'></i>
+                     </span>{' '}
+                     <div className='hover-text1'>
+                        <h4>Fifth Birthday</h4>
+                        <p>We celebrated in finland 2020. Dress bought from her grandparents.Cake made by amma</p>
+                      </div>
+                    </a>{' '}
+                  </div>
+                </div>
+              </div>
+            
+              <div className='col-xs-6 col-md-3'> 
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
+                    {' '}
+                    <a
+                      href='img/birthdays/birthday5.jpg'
+                      title='Project Title'
+                      data-lightbox-gallery='drawing2'
+                    >
+                      <span>
+                      <i className='fa fa-child'></i>
+                     </span>{' '}
+                     <div className='hover-text1'>
+                        <h4>Sixth Birthday</h4>
+                        <p>We celebrated in finland 2021. Dress bought from her grandparents.Cake made by amma</p>
+                      </div>
+                    </a>{' '}
+                  </div>
+                </div>
+              </div>
+
+
+            </div>
           </div>
         </div>
-      </div>
+    
     )
   }
